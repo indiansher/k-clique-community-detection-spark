cb95a44a4df3df1507fd80bd271da24d294b0d6c
Amit Sukhpal <amitsu12@gmail.com>
2019-11-15 05:00:18 +0800

    Trying small_star / large_star

--- cpm-spark.py
+++ cpm-spark.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import datetime;
+import csv
 import collections;
 from pyspark import SparkConf, SparkContext
 
@@ -19,6 +20,7 @@ k = sys.argv[2]
 # Step 1 (Generate Adjacency list)
 adjacencyList = edges \
     .map(lambda l: re.split(r' ', l)) \
+    .map(lambda x: (int(x[0]), int(x[1]))) \
     .map(lambda x: (x[0], [x[1]]) if x[0] < x[1] else (x[1], [x[0]])) \
     .reduceByKey(lambda a, b: a + b)
 
@@ -39,15 +41,25 @@ for i in range(2, int(k) + 1):
         .flatMap(lambda x: [(x[0] + [x[1][j]], x[1][j + 1:]) for j in range(len(x[1]))]) \
         .filter(lambda x: is_edge_exists(x[0][-1], x[0][:-1]))
 
-cliques = cliques.keys();
+cliques = cliques \
+    .keys() \
+    .map(lambda x: tuple(x))
 
-# Save cliques
-# cliques.keys().repartition(1).saveAsTextFile(k + "-cliques-" + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
+cliquesMap = {k: v for v, k in enumerate(cliques.collect())}
+totalCliques = len(cliquesMap)
 
-cliques = cliques.collect();
+# Save clique Map
+cliquesMapFile = open(k + "-cliques-map-" + datetime.datetime.now().strftime('%Y%m%d-%H%M%S') + ".csv", "w")
+w = csv.writer(cliquesMapFile)
+for key, val in cliquesMap.items():
+    w.writerow([val, key])
+cliquesMapFile.close()
 
-upper_overlap_matrix_list = []
+# Broadcast Cliques Map
+cliquesMap_bc = sc.broadcast(cliquesMap)
 
+
+# Find k-1 Adjacent cliques
 
 def is_k_1_adjacent(clique1, clique2):
     adjacency_nodes_counter = collections.Counter(clique1)
@@ -56,49 +68,45 @@ def is_k_1_adjacent(clique1, clique2):
     return len(overlap_counter) == int(k)
 
 
-for i in range(len(cliques)):
-    for j in range(i, len(cliques)):
-        upper_overlap_matrix_list.append(is_k_1_adjacent(cliques[i], cliques[j]))
+k_1_adjacency_edges = cliques \
+    .flatMap(lambda x: [((x[:j] + x[j + 1:]), [cliquesMap_bc.value[x]]) for j in range(len(x))]) \
+    .reduceByKey(lambda a, b: a + b) \
+    .values() \
+    .flatMap(lambda x: [(v, x) for v in x]) \
+    .reduceByKey(lambda a, b: a + b) \
+    .flatMap(lambda x: [(x[0], v) for v in x[1] if x[0] > v])
+
+print(k_1_adjacency_edges.take(10))
 
 
+# Find connected components:
+# Kiveris, R., Lattanzi, S., Mirrokni, V., Rastogi, V. and Vassilvitskii, S., 2014, November.
+# Connected components in mapreduce and beyond. In Proceedings of the ACM Symposium on Cloud Computing (pp. 1-13). ACM.
+# Vancouver
 
-any_merge = True;
-while any_merge:
-    any_merge = False;
-
-print(cliques.take(10))
-
-## Finding K-1 Adjacent cliques and joining them to form communities
-k_1_adjacent_nodes = cliques \
-    .flatMap(lambda x: [(x[:j] + x[j + 1:]) for j in range(len(x))]) \
-    .map(lambda x: tuple(x)) \
-    .distinct()
-k_1_adjacent_nodes_bc = sc.broadcast(k_1_adjacent_nodes.collect())
+def small_star(edges_rdd):
+    return edges_rdd \
+        .map(lambda x: (x[0], [x[1]]) if x[0] > x[1] else (x[1], [x[0]])) \
+        .reduceByKey(lambda a, b: a + b) \
+        .map(lambda x: (min(x[1]), x[1])) \
+        .flatMap(lambda x: [(v, x[0]) for v in x[1]])
 
 
-def is_k_1_overlap(adjacency_nodes, community):
-    adjacency_nodes_counter = collections.Counter(adjacency_nodes)
-    community_counter = collections.Counter(community)
-    overlap_counter = adjacency_nodes_counter & community_counter
-    return len(overlap_counter) == len(adjacency_nodes_counter)
+def large_star(edges_rdd):
+    return edges_rdd \
+        .flatMap(lambda x: [(x[0], [x[1]]), (x[1], [x[0]])]) \
+        .reduceByKey(lambda a, b: a + b) \
+        .map(lambda x: (min(x[1] + [x[0]]), x[1])) \
+        .flatMap(lambda x: [(v, x[0]) for v in x[1] if v > x[0]])
 
 
-final_communities = []
-community_size = int(k)
+s = k_1_adjacency_edges
+t = large_star(small_star(s))
+print(t.take(10))
+while not s.subtract(t).isEmpty():
+    s = t
+    t = large_star(small_star(s))
+    print(t.take(10))
 
-while not cliques.isEmpty():
-    communities = cliques \
-        .flatMap(lambda x: [(nodes, x) for nodes in k_1_adjacent_nodes_bc.value if is_k_1_overlap(nodes, x)]) \
-        .reduceByKey(lambda a, b: list(set(a) | set(b))) \
-        .values() \
-        .collect()
-
-    # cliques = communities_bc.value.filter(lambda x: len(x) > community_size)
-    # extracted_communities = communities_bc.value.filter(lambda x: len(x) == community_size)
-    cliques = sc.parallelize(filter(lambda x: len(x) > community_size, communities))
-    final_communities += filter(lambda x: len(x) == community_size, communities)
-    community_size += 1
-
-print(final_communities)
 
 sc.stop()
